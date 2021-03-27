cd469f4d8123d8b24659a3a5e1ca276327861cea
aachal28 <77334130+aachal28@users.noreply.github.com>
2021-03-27 05:34:10 -0700

    assignment1 submitted

---
+++ letupgrade assignment/assignment.js
@@ -0,0 +1,8 @@
+var ele=document.getElementsByTagName('h1');
+console.log(ele[0].innerHTML);
+
+var ele=document.getElementsByClassName('inp');
+console.log(ele[0].value);
+
+var ele=document.getElementById('web');
+console.log(ele);
